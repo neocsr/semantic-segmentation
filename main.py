83c1faac1730771aeceb8b8471419e5d108a8a53
Christian <neocsr@gmail.com>
2017-09-11 07:05:56 -0700

    Add kernel_initializer

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         filters=num_classes,
         kernel_size=1,
         padding='same',
+        kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01),
         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     layer7_up = tf.layers.conv2d_transpose(
@@ -74,6 +75,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         kernel_size=4,
         strides=2,
         padding='same',
+        kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01),
         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     layer4_conv = tf.layers.conv2d(
@@ -81,6 +83,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         filters=num_classes,
         kernel_size=1,
         padding='same',
+        kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01),
         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     layer4_skip = tf.add(layer7_up, layer4_conv)
@@ -91,6 +94,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         kernel_size=4,
         strides=2,
         padding='same',
+        kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01),
         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     layer3_conv = tf.layers.conv2d(
@@ -98,6 +102,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         filters=num_classes,
         kernel_size=1,
         padding='same',
+        kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01),
         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     layer3_skip = tf.add(layer4_up, layer3_conv)
@@ -108,6 +113,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
         kernel_size=16,
         strides=8,
         padding='same',
+        kernel_initializer=tf.truncated_normal_initializer(stddev = 0.01),
         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     return layer3_up
